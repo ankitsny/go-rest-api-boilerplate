5c4e6e5834f8d890216d9301d45dbfba0dde95f0
Ankit Kumar <ankit_sony3@hotmail.com>
2018-03-08 14:41:20 +0530

    id field

--- models/user.go
+++ models/user.go
@@ -1,12 +1,15 @@
 package models
 
+import "gopkg.in/mgo.v2/bson"
+
 // User :
 type User struct {
-	FirstName string `json:"firstName,omitempty" bson:"firstName,omitempty" form:"firstName"`
-	LastName  string `json:"lastName,omitempty" bson:"lastName,omitempty" form:"lastName"`
-	Email     string `json:"email,omitempty" bson:"email,omitempty" form:"email"`
-	Password  string `json:"-" bson:"password"`
-	Mobile    int64  `json:"mobile,omitempty" bson:"mobile,omitempty" form:"mobile"`
+	ID        bson.ObjectId `json:"id" bson:"_id"`
+	FirstName string        `json:"firstName,omitempty" bson:"firstName,omitempty" form:"firstName"`
+	LastName  string        `json:"lastName,omitempty" bson:"lastName,omitempty" form:"lastName"`
+	Email     string        `json:"email,omitempty" bson:"email,omitempty" form:"email"`
+	Password  string        `json:"-" bson:"password"`
+	Mobile    int64         `json:"mobile,omitempty" bson:"mobile,omitempty" form:"mobile"`
 }
 
 // Validate : validate function to validate the User docs
