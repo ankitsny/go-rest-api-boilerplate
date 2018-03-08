1756203049976f196227db067545c661f5d1083d
Ankit Kumar <ankit_sony3@hotmail.com>
2018-03-08 15:45:00 +0530

    created basic http server

--- main.go
+++ main.go
@@ -2,8 +2,45 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	fmt.Println("Hello World")
+
+	// TODO: load app configs here
+
+	// TODO: Connect to database, app level
+
+	// TODO: Connect redis store, app level
+
+	// TODO: Create logger instance? if required
+
+	// start the server
+	address := fmt.Sprintf(":%v", 8080)                    // get the port number from the app config
+	log.Printf("server %v is started at %v\n", 1, address) // get the app version from the app config
+	panic(http.ListenAndServe(address, buildRoutes()))
+}
+
+func buildRoutes() *mux.Router {
+	// create instance of mux router
+	r := mux.NewRouter()
+
+	// Initialize not found handler
+	r.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(404)
+		w.Write([]byte("Rosource not found"))
+	})
+
+	// Set path prefix/route group
+	r.PathPrefix("v1")
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		fmt.Fprintf(w, "Hello World")
+	}).Methods("GET")
+
+	return r
 }
