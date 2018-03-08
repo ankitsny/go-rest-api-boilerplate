efdc94a5bb1cf0c5bde96812e26cc300e898b2c2
Ankit Kumar <ankit_sony3@hotmail.com>
2018-03-08 18:06:25 +0530

    created request scope

---
+++ app/requestscope.go
@@ -0,0 +1,93 @@
+package app
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/Sirupsen/logrus"
+	mgo "gopkg.in/mgo.v2"
+)
+
+// RequestScope contains the application-specific information that are carried around in a request.
+type RequestScope interface {
+	Logger
+	// UserID returns the ID of the user for the current request
+	UserID() string
+	// SetUserID sets the ID of the currently authenticated user
+	SetUserID(id string)
+	// RequestID returns the ID of the current request
+	RequestID() string
+	// Tx returns the currently active database transaction that can be used for DB query purpose
+	DB() *mgo.Database
+	// Now returns the timestamp representing the time when the request is being processed
+	Now() time.Time
+	// Set All params
+	SetParams(params map[interface{}]interface{})
+	// Get All Params
+	GetParams() map[interface{}]interface{}
+	// SetBody
+	SetBody(body []byte)
+	// GetBody
+	GetBody() []byte
+}
+
+type requestScope struct {
+	Logger                                // the logger tagged with the current request information
+	now       time.Time                   // the time when the request is being processed
+	requestID string                      // an ID identifying one or multiple correlated HTTP requests
+	userID    string                      // an ID identifying the current user
+	db        *mgo.Database               // Database object
+	params    map[interface{}]interface{} // url Params Params
+	body      []byte                      // Body
+}
+
+func (rs *requestScope) UserID() string {
+	return rs.userID
+}
+
+func (rs *requestScope) SetUserID(id string) {
+	rs.Logger.SetField("UserID", id)
+	rs.userID = id
+}
+
+func (rs *requestScope) RequestID() string {
+	return rs.requestID
+}
+
+func (rs *requestScope) Now() time.Time {
+	return rs.now
+}
+
+func (rs *requestScope) DB() *mgo.Database {
+	return rs.db
+}
+
+func (rs *requestScope) SetParams(params map[interface{}]interface{}) {
+	rs.params = params
+}
+
+func (rs *requestScope) GetParams() map[interface{}]interface{} {
+	return rs.params
+}
+
+func (rs *requestScope) SetBody(body []byte) {
+	rs.body = body
+}
+
+func (rs *requestScope) GetBody() []byte {
+	return rs.body
+}
+
+// newRequestScope creates a new RequestScope with the current request information.
+func newRequestScope(now time.Time, logger *logrus.Logger, request *http.Request) RequestScope {
+	l := NewLogger(logger, logrus.Fields{})
+	requestID := request.Header.Get("X-Request-Id")
+	if requestID != "" {
+		l.SetField("RequestID", requestID)
+	}
+	return &requestScope{
+		Logger:    l,
+		now:       now,
+		requestID: requestID,
+	}
+}
---
+++ daos/user.go
@@ -0,0 +1,9 @@
+package daos
+
+// UserDAO :
+type UserDAO struct{}
+
+// NewUserDao : create instance of userdao
+func NewUserDao() *UserDAO {
+	return &UserDAO{}
+}
